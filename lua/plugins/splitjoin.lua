d98f915b33b06f2bc895d8490474e56d217d9b0c
C33 <c33@gmail.com>
2024-05-08 01:10:26 -0600

    Update splitjoin

--- lua/plugins/splitjoin.lua
+++ lua/plugins/splitjoin.lua
@@ -1,18 +1,23 @@
 return {
-  "Wansmer/treesj",
-  event = "BufEnter",
-  cmd = { "TSJToggle", "TSJSplit", "TSJJoin" },
-  opts = {
-    use_default_keymaps = false,
-    max_join_length = 840,
-  },
-  dependencies = {
-    "AstroNvim/astrocore",
-    ---@type AstroCoreOpts
+  {
+    "Wansmer/treesj",
+    event = "BufEnter",
+    cmd = { "TSJToggle", "TSJSplit", "TSJJoin" },
     opts = {
-      mappings = {
-        n = {
-          ["zj"] = "<Cmd>TSJToggle<CR>",
+      use_default_keymaps = false,
+      max_join_length = 840,
+    },
+    dependencies = {
+      "AstroNvim/astrocore",
+      ---@type AstroCoreOpts
+      opts = {
+        mappings = {
+          n = {
+            ["zj"] = "<Cmd>TSJToggle<CR>",
+          },
+          v = {
+            ["<C-j>"] = "<Cmd>'<,'>join<CR>",
+          },
         },
       },
     },
