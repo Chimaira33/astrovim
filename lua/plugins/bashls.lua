0ec7834079ee2a0e675cc8ebee242f5be20a4593
C33 <c33@gmail.com>
2024-06-01 21:12:52 -0600

    Update rustaceanvim and disable unused bashls settings

--- lua/plugins/bashls.lua
+++ lua/plugins/bashls.lua
@@ -3,19 +3,16 @@ return {
   "AstroNvim/astrolsp",
   opts = function(_, opts)
     table.insert(opts.servers, "bashls")
-    opts.config = {
+    --[[ opts.config = {
       bashls = {
         settings = {
-          bashIde = {
-            backgroundAnalysisMaxFiles = 0,
-            enableSourceErrorDiagnostics = false,
-            includeAllWorkspaceSymbols = false,
-            inclueAllWorkspaceSymbols = false,
-            logLevel = "error",
-            shellcheckArguments = "--exclude=SC1091,SC2016,SC2034,SC2043,SC2117,SC2139,SC2154,SC2188,SC2317 --enable=require-double-brackets,deprecate-which,avoid-nullary-conditions",
-          },
+          backgroundAnalysisMaxFiles = 100,
+          -- enableSourceErrorDiagnostics = true,
+          includeAllWorkspaceSymbols = true,
+          logLevel = "error",
+          shellcheckArguments = "--exclude=SC1091,SC2016,SC2034,SC2043,SC2117,SC2139,SC2154,SC2188,SC2317 --enable=require-double-brackets,deprecate-which,avoid-nullary-conditions",
         },
       },
-    }
+    } ]]
   end,
 }
