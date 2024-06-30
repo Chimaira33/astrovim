4c7aaccd473180fb8ebc0b5286681bd3ce7e8776
Chimaira33 <c33@gmail.com>
2024-06-30 07:33:02 -0600

    New lazy.nvim freaks out with providers.lua in plugin directory.

--- init.lua
+++ init.lua
@@ -9,5 +9,6 @@ vim.opt.rtp:prepend(lazypath)
 if not pcall(require, "lazy") then vim.api.nvim_echo({{("Unable to load lazy from: %s\n"):format(lazypath), "ErrorMsg"}, {"Press any key to exit...", "MoreMsg"}}, true, {}) vim.fn.getchar() vim.cmd.quit() end
 
 require("lazy_setup")
+require("providers")
 require("user.osc")
 require("user.conf")
